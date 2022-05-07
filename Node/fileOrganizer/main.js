a964b29674062ae655fe03e67dedd40f56cc3c1d
dax.batra88@gmail.com <dax.batra88@gmail.com>
2022-05-07 16:07:32 +0530

    File Organizer Started

---
+++ Node/fileOrganizer/main.js
@@ -0,0 +1,23 @@
+
+
+let inputArr = process.argv.slice(2);
+let command = input[0];
+
+switch(command)
+{
+    case "tree":
+        //call tree function
+        break;
+    
+    case "organize":
+        //call organize function
+        break;
+
+    case "help":
+        //call help function
+        break;
+
+    default:
+        console.log("Command not recognized :/");
+        break;
+}
